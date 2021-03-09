4e91b18451a4be03ed92b85b5a4e65435eed7b59
sumin <sue4869@naver.com>
2021-03-09 19:06:13 +0900

    no message

---
+++ 1_input함수.py
@@ -0,0 +1,22 @@
+a = 10
+a = a + 10
+print(a)
+
+print("Life is too short, You need Python.")
+print("돈 많은")
+print("백수가 되고 싶다...")
+# 주석 달때는 ctrl + /
+
+a = input("입력 >>>")
+b = input("입력 >>>")
+print(a , b) # 문자열 띄어쓰기 할때는 ,를 쓴다
+
+a = int(input("입력 >>>"))
+b = int(input("입력 >>>"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b) # // : 두수를 나눈 후 몫을 알려줌
+
+a = int(input("입력 >>>"))
+print(a%2)
